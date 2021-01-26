b802fdfd3b32e54e34479341a7665a45b49334f5
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-01-25 22:20:52 -0300

    🚧 PROGRESS: Validation with Youp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
 import { Form } from '@unform/web';
+import * as Yup from 'yup'
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,9 +12,20 @@ import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
 
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data): Promise<void> => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return(
     <Container>
