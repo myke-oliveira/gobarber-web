3d3517298329923d71226636dd60b7951a864e20
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-01-26 21:22:09 -0300

    ✨ NEW FEATURE: Add AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 import SignIn from "./pages/SignIn";
 import GlobalStyle from './styles/global'
 
+import AuthContext from './context/AuthContext';
+
+AuthContext.Provider
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Myke' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
